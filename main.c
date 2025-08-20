052f4e3f707ea79d7e0e3982f43314648fe89eeb
Sunny Pritchard <pritchardsunny@gmail.com>
2025-08-20 14:55:15 +0200

    feat(simple shell v0.1): Implement basic shell functionality:
    - Added initial shell loop to read and execute commands
    - Implemented basic command parsing and execution logic
    - Integrated support for running simple commands (e.g., /bin/ls)

--- main.c
+++ main.c
@@ -1,8 +1,31 @@
+#include "shell.h"
+
 /**
 * main - Entry point for simple_shell
 * Return: 0 on success
 */
 int main(void)
 {
+	char *command;
+
+	while (1)
+	{
+		if (isatty(STDIN_FILENO))
+		{
+			printf("$ ");
+			fflush(stdout);
+		}
+
+		command = read_command();
+
+		if (command == NULL) /* Handle Ctrl+D (EOF) */
+		{
+			break;
+		}
+
+		execute_command(command);
+		free(command);
+	}
+
 	return (0);
 }
